1ca9a60f3f0fa3e7bffc219741e0a71ba4380473
RGabaydullov <rsht123g@gmail.com>
2018-03-23 19:03:22 +0300

    Instagram updateFollower issue with dup key fixed

--- middleware/api/methods/updateFollower.js
+++ middleware/api/methods/updateFollower.js
@@ -4,14 +4,18 @@ module.exports = function(models) {
     return method(models, updateFollower)
 };
 
-function updateFollower(follower) {//console.log('    follower: %o', follower);
+function updateFollower(follower) {console.log('follower: %o', follower);
     let models = this,
         InstagramFollowers = models.InstagramFollowers;
 
     return new Promise((resolve, reject) => {
         InstagramFollowers.findOneAndUpdate(follower, { $set: follower }, { upsert: true }, (err, user) => {
             if(!!err) { return reject(err) }
-    
+
+            if(!follower.instagramId) {
+                return resolve(user)
+            }
+
             user = !!user ? user : new InstagramFollowers({ $set: follower });
 
             //console.log('\x1b[32m%s\x1b[0m %o', 'InstagramFollower:', user);
