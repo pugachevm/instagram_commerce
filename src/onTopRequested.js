4054fabb895994570de443828904153356a744cb
RGabaydullov <rsht123g@gmail.com>
2018-03-26 18:51:53 +0300

    Top20 functionality enabled

--- src/onTopRequested.js
+++ src/onTopRequested.js
@@ -4,13 +4,25 @@ const BUTTONS = JSON.parse(fs.readFileSync('./src/buttons.json', 'utf-8'));
 const MESSAGES = JSON.parse(fs.readFileSync('./src/messages.json', 'utf-8'));
 
 module.exports = function($api, context) {
+    let $bot = this,
+        $messages = [ MESSAGES.topUsers.title ],
+        $unit = MESSAGES.topUsers.point;
 
-    let $bot = this;
-    
-    return context.answerCbQuery()
-        .then(() => $bot.editMessage(
-            context,
-            'Top20 requested',
-            $bot.getKeyboard(BUTTONS.menuBack)
-        ))
+    $api.getUsersByCriteria({ preCountedPoints: -1})
+        .then((users) => {
+            users.forEach((user, i) => {
+                let { firstName, preCountedPoints } = user;
+                firstName = firstName || 'Аноним';
+
+                $messages.push(`*${i+1}.*${firstName}: *${preCountedPoints}* ${$unit}`)
+            });
+
+            return context.answerCbQuery()
+                .then(() => $bot.editMessage(
+                    context,
+                    $messages.join(''),
+                    $bot.getKeyboard(BUTTONS.menuBack)
+                ))
+        })
+        .catch(console.error);
 };
