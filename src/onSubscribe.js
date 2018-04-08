511750df0182fe9e55c87763dc98a89afcf38b60
Rishat Gabaydullov <rsht123g@gmail.com>
2018-04-08 15:24:18 +0300

    Content issues

--- src/onSubscribe.js
+++ src/onSubscribe.js
@@ -5,13 +5,11 @@ const MESSAGES = JSON.parse(fs.readFileSync('./src/messages.json', 'utf-8'));
 
 module.exports = function($api) {
     let $bot = this;
-
-    $bot.send(MESSAGES.subscribe);
     
     $bot.send(
         MESSAGES.activated,
         $bot.getKeyboard(BUTTONS.subscribe)
     );
 
-    $bot.send(MESSAGES.activationNotation, $bot.getKeyboard(BUTTONS.callAmSubscribed, 'static'))
+    $bot.send(MESSAGES.activationNotation, $bot.getKeyboard(BUTTONS.callAmSubscribed))
 };
