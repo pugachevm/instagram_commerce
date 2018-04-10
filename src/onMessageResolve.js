d49786f6206f5b44198889dfc7ea509fa2a49642
RGabaydullov <rsht123g@gmail.com>
2018-04-10 16:15:59 +0300

    Messageresolver fixes

--- src/onMessageResolve.js
+++ src/onMessageResolve.js
@@ -30,17 +30,17 @@ module.exports = function($api, chatId, context) {
     switch(param) {
         case 'new':
             messageResolver(_message.id, _message);
-            $message.push(_message.text, ' has been added');
+            $message.push([ _message.text, ' было *добавлено*' ].join(''));
             break;
         case 'delete':
             messageResolver(args);
-            $message.push(args, ' has been deleted')
+            $message.push([ args, ' было *удалено*' ].join(''));
             break;
         case 'all':
             Object.keys(STORAGE_MESSAGES).forEach(id => {
                 let _message = STORAGE_MESSAGES[id];
-                $message.push([ id, _message.text ].join(':'))
-            })
+                $message.push([ '*'+ id +'*', _message.text ].join(' : '))
+            });
             break;
         case 'help':
 
@@ -59,7 +59,7 @@ function messageResolver(id, message) {
     _message && clearTimeout(_message.to);
 
     if(message) {
-        message.to = setTimeout(() => { dispatchAll(message) }, message.ms)
+        message.to = setTimeout(() => { dispatchAll(message) }, message.ms);
         return STORAGE_MESSAGES[id] = message;
     }
 
@@ -79,12 +79,10 @@ function formatArgs(args) {
         timeEnd = +(new Date(2018, month-1, day, hours, minutes)),
         _date = { day, month, hours, minutes };
     
-    message = args.substr(12) || '';
-    
     return {
-        ms: Math.max(0, timeEnd - timeNow) + 1000,
+        ms: Math.max(0, timeEnd - timeNow),
         id: +[ day, month, hours, minutes ].join(''),
-        text: message
+        text: args.substr(12) || ''
     }
 }
 
